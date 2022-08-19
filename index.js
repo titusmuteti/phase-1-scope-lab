62af8541677c176e7c47af89d6493cfd1a8e8310
Titus Mumo <titusmumo@gmail.com>
2022-08-19 19:58:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+//first test
+var customerName = "bob";
+
+//Second test
+function  upperCaseCustomerName () {
+     customerName = 'BOB';
+}
+
+//Third test
+var setBestCustomer= function () {
+    window.bestCustomer = 'not bob';
+}
+
+//Fourth test
+var overwriteBestCustomer= function () {
+    bestCustomer = 'maybe bob';
+}
+
+//Fifth test
+const leastFavoriteCustomer = 'claire';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'job';
+}
